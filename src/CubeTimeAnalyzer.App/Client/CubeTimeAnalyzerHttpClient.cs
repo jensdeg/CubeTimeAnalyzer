426ffa3de9e263656c597c52b2b29864114eb507
jensdeg <jensdegraaf88@gmail.com>
2025-08-10 12:27:24 +0200

    base import page

--- src/CubeTimeAnalyzer.App/Client/CubeTimeAnalyzerHttpClient.cs
+++ src/CubeTimeAnalyzer.App/Client/CubeTimeAnalyzerHttpClient.cs
@@ -5,6 +5,7 @@ namespace CubeTimeAnalyzer.App.Client
     public class CubeTimeAnalyzerHttpClient(HttpClient http)
     {
         private const string AveragesEndpoint = "Analyze/Averages";
+        private const string ImportEndpoint = "Import";
 
         public async Task<Ao5Model[]> GetAo5sAsync()
         {
@@ -17,6 +18,19 @@ namespace CubeTimeAnalyzer.App.Client
                 Console.WriteLine($"An error occurred while fetching Ao5s: {ex.Message}");
                 return [];
             }
+        }
+
+        public async Task ImportTimes(IFormFile file)
+        {
+            try
+            {
+                await http.PostAsJsonAsync(ImportEndpoint, file);
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"An error occurred while Importing file: {ex.Message}");
+            }
+
         }
     }
 }
